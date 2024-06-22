ea3a1f717f55ff2b9dce04fb170446026d601240
Dalton Williams <daltonstwilliams@gmail.com>
2024-06-22 17:39:54 -0500

    2nd implementation, quite the success :)

--- Go/README.md
+++ Go/README.md
@@ -1,6 +1,8 @@
 # Go
 
-## 1st Completion - 2m 30.66s
+**Current Best: 80.785s**
+
+## 1st implementation - 2m 30.66s (150.66)
 
 Naive implementation, timed with zsh `time` command. 
 - 149.50s attributed to user, 5.78s attributed to system
@@ -11,3 +13,17 @@ Naive implementation, timed with zsh `time` command.
 - Waited until the end to divide the average
 - Put all the names in a slice, sorted that, then looped over that to print everything out alphabetically
 - Formatting was handled with Sprintf, formatting all the floats with `%.1f` to round them
+
+## 2nd implementation - 1m 20.785s (80.785)
+
+Note: From here on, I'm going to be testing these using Go's built in benchmarking. I reran the first test using it, and it was basically the same as zsh`time`, so I don't think it matters to change the first one.
+
+Wow what a change! I managed to cut the time in half before even implementing any coprocessing.
+
+So what changed with this implementation?
+- I learned more deeply about how structs worked. I wasn't confident enough in them to use them in my baseline implementation, so I just... didn't
+  - I created a `city` struct that was comprised of a `count` int, and a `total`, `min`, and `max` float64
+  - I then created a `mapHandler` struct that was just a map[string]city, to keep a mapping of city name to city info
+
+
+*Nothing* changed as far as float parsing, calculations, or formatting is concerned, nor did I implement any goroutines. Plenty of optimization to be had!
--- Go/main.go
+++ Go/main.go
@@ -3,12 +3,67 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"sync"
+
 	// "math"
 	"os"
+	"runtime/pprof"
 	"sort"
 	"strconv"
 	"strings"
 )
+
+type city struct {
+	count int
+	total float64
+	min   float64
+	max   float64
+}
+
+func (c city) process(in float64) {
+	c.count += 1
+	c.total += in
+	if in < c.min {
+		c.min = in
+		return
+	} else if in > c.max {
+		c.max = in
+	}
+}
+
+func (c city) getAvg() float64 {
+	return c.total / float64(c.count)
+}
+
+type mapHandler struct {
+	mapping map[string]city
+}
+
+func (handler mapHandler) process(name string, in string) {
+	c, exist := handler.mapping[name]
+	if !exist {
+		c = city{}
+	}
+
+	float, err := strconv.ParseFloat(in, 64)
+	check(err)
+
+	c.process(float)
+}
+
+func (handler mapHandler) getSortedKeys() []string {
+	mapping := handler.mapping
+	keys := make([]string, 0, len(mapping))
+	for k := range mapping {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+func (handler mapHandler) getCity(name string) city {
+	return handler.mapping[name]
+}
 
 func check(e error) {
 	if e != nil {
@@ -17,41 +72,39 @@ func check(e error) {
 }
 
 func main() {
+	file, err := os.Create("1BRC.prof")
+	check(err)
+	pprof.StartCPUProfile(file)
+	Run1BRC()
+	pprof.StopCPUProfile()
+}
+
+const ChunkSize = 2048
+
+func Run1BRC() {
 	input, err := os.Open("../measurements.txt")
 	check(err)
 	defer input.Close()
 
-	counts := make(map[string]int)
-	totals := make(map[string]float64)
-	mins := make(map[string]float64)
-	maxs := make(map[string]float64)
+	handler := mapHandler{}
 
 	scanner := bufio.NewScanner(input)
 	for scanner.Scan() {
 		line := strings.Split(scanner.Text(), ";")
-		city := line[0]
-		temp, _ := strconv.ParseFloat(line[1], 32)
-
-		totals[city] += temp
-		counts[city] += 1
-
-		if temp < mins[city] {
-			mins[city] = temp
-			continue
-		} else if temp > maxs[city] {
-			maxs[city] = temp
-		}
-
+		city, temp := line[0], line[1]
+		handler.process(city, temp)
 	}
 
-	keys := make([]string, 0, len(counts))
-	for k := range counts {
-		keys = append(keys, k)
+	keys := handler.getSortedKeys()
+
+	for k := range keys {
+		c := handler.getCity(keys[k])
+
+		fmt.Println(fmt.Sprintf("%s %.1f %.1f %.1f", keys[k], c.min, c.getAvg(), c.max))
 	}
-	sort.Strings(keys)
-	for _, k := range keys {
-		avg := totals[k] / float64(counts[k])
-		fmt.Println(fmt.Sprintf("%s %.1f %.1f %.1f", k, mins[k], avg, maxs[k]))
-		// fmt.Println(k, mins[k], math.Floor(avg*10)/10, maxs[k])
-	}
+}
+
+// For when I implement goroutines. Presently just want to see if using structs helps anything
+func ProcessRows(row string, wg *sync.WaitGroup) {
+	defer wg.Done()
 }
