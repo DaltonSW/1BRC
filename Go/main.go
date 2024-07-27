69c5cb7c0b08fb34c1346cbd1a86566af717558a
Dalton Williams <daltonstwilliams@gmail.com>
2024-07-27 10:36:09 -0500

    Pointer city

--- Go/main.go
+++ Go/main.go
@@ -62,7 +62,7 @@ type city struct {
 	Min int64
 	Max int64
 	Sum int64
-	Cnt int64
+	Cnt int
 }
 
 func Run1BRC(bufferSize int, filepath string) {
@@ -73,7 +73,7 @@ func Run1BRC(bufferSize int, filepath string) {
 
 	lineBuffer := make([]byte, bufferSize)
 	remainder := make([]byte, 0)
-	cityChan := make(chan map[string]city)
+	cityChan := make(chan map[string]*city)
 
 	// Open measurements
 	input, err = os.Open(filepath)
@@ -94,8 +94,7 @@ func Run1BRC(bufferSize int, filepath string) {
 		bytesRead := lineBuffer[:num]
 		chunk := append(remainder, bytesRead...)
 
-		// ISSUE: Byte splitting like this is taking 13.7%
-		splitChunk := bytes.Split(chunk, []byte{'\n'})
+		splitChunk := bytes.Split(chunk, []byte{'\n'}) // PERF: 11.7% -- bytes.Split
 
 		chunkLineCount := len(splitChunk) - 1
 		remainder = splitChunk[chunkLineCount]
@@ -121,12 +120,12 @@ func Run1BRC(bufferSize int, filepath string) {
 		close(cityChan)
 	}()
 
-	totals := make(map[string]city)
+	totals := make(map[string]*city)
 	for chunk := range cityChan {
 		for name, inCity := range chunk {
 			c, ok := totals[name]
 			if !ok {
-				totals[name] = city{
+				totals[name] = &city{
 					Min: inCity.Min,
 					Max: inCity.Max,
 					Sum: inCity.Sum,
@@ -138,7 +137,6 @@ func Run1BRC(bufferSize int, filepath string) {
 			c.Max = max(c.Max, inCity.Max)
 			c.Sum += inCity.Sum
 			c.Cnt += inCity.Cnt
-			totals[name] = c
 		}
 	}
 
@@ -156,13 +154,15 @@ func Run1BRC(bufferSize int, filepath string) {
 	}
 }
 
-func ProcessChunk(lines [][]byte, cityChan chan map[string]city) {
-	chunkMap := make(map[string]city)
+func ProcessChunk(lines [][]byte, cityChan chan map[string]*city) {
+	chunkMap := make(map[string]*city)
 	for _, line := range lines {
 		lineLen := len(line)
 		if lineLen < 2 {
 			break
 		}
+
+		// TODO: Combine finding the semicolon and parsing the number into one loop backwards
 
 		var semi int
 		index := lineLen - 4 // 4 back is the first one that could possibly be a semicolon
@@ -174,30 +174,29 @@ func ProcessChunk(lines [][]byte, cityChan chan map[string]city) {
 			semi = index - 2
 		}
 
+		// PERF: 22.5% -- slicebytetostring
 		name := string(line[:semi])
-		byteTemp := append(line[semi+1:lineLen-2], line[lineLen-1])
+		byteTemp := string(append(line[semi+1:lineLen-2], line[lineLen-1]))
 
-		// ISSUE: ParseInt is taking 7.1%
-		// IDEA:  Manually parse the temp bytes into an int directly, skip strings entirely for temp
-		temp, _ := strconv.ParseInt(string(byteTemp), 10, 64)
+		temp, _ := strconv.ParseInt(byteTemp, 10, 64) // PERF: 9% -- ParseInt
 
+		// PERF: 26.3% -- mapaccess2_faststr
 		c, ok := chunkMap[name]
-
 		if !ok {
-			c = city{
+			chunkMap[name] = &city{
 
 				Cnt: 1,
 				Max: temp,
 				Min: temp,
 				Sum: temp,
 			}
-		} else {
-			c.Cnt++
-			c.Max = max(c.Max, temp)
-			c.Min = min(c.Min, temp)
-			c.Sum += temp
+			continue
 		}
-		chunkMap[name] = c
+
+		c.Cnt++
+		c.Max = max(c.Max, temp)
+		c.Min = min(c.Min, temp)
+		c.Sum += temp
 	}
 	cityChan <- chunkMap
 }
